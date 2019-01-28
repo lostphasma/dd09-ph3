cdfc6047fab3e1074cdef6975083f88b4454007e
lostphasma <alabardadellanebulosa@gmail.com>
2019-01-28 11:32:18 +0100

    Sliders - Update

--- Slider-2/main.js
+++ Slider-2/main.js
@@ -1,32 +1,37 @@
 var point_positions = [];
+//multiple lines non serve
 // var json_data_muliple_lines = [[{"id": "82","x": "50","y": "50"}, {"id": "83","x": "25","y": "110"}, {"id": "97","x": "90","y": "150"}, {"id": "98","x": "150","y": "224"}, {"id": "99","x": "250","y": "150"}, {"id": "100","x": "300","y": "200"}, {"id": "100","x": "320","y": "230"}],[{"id": "1","x": "120","y": "60"}, {"id": "2","x": "30","y": "150"}, {"id": "3","x": "120","y": "170"}, {"id": "4","x": "180","y": "260"}, {"id": "5","x": "300","y": "250"}]];
-var json_data = [{
+
+var json_data = [{"x": "50","y": "300"}, {"x": "150","y": "300"}, {"x": "250","y": "300"}, {"x": "350","y": "300"}, {"x": "450","y": "300"}, {"x": "550","y": "300"}, {"x": "650","y": "300"}];
+var curva_arrivo = [{
     "x": "50",
     "y": "300"
 }, {
     "x": "150",
-    "y": "300"
+    "y": "600"
 }, {
     "x": "250",
-    "y": "300"
+    "y": "600"
 }, {
     "x": "350",
-    "y": "300"
+    "y": "600"
 }, {
     "x": "450",
-    "y": "300"
+    "y": "600"
 }, {
     "x": "550",
-    "y": "300"
+    "y": "600"
 }, {
     "x": "650",
     "y": "300"
 }];
+console.log("dati iniziali");
+console.log(json_data);
 
 var handleRadius = 6;
 var handleOffset = 35;
 
-//-----Pusha le posizioni dei punti nell'array nel dato momento
+//-----Pusha le posizioni dei punti nell'array point_position nel dato momento
 $.each(json_data, function(i, item) {
     line_response = json_data[i];
     // var line_pi_id = line_response.line_pi_id;
@@ -48,7 +53,7 @@ function curves_init(point_positions) {
         type: 'Q',
         points: point_positions
     }];
-    console.log("curves", curves);
+    //console.log("curves", curves);
 
     //crea dei gruppi nell'svg e da attributi
     var controlLineLayer = svg.append('g').attr('class', 'control-line-layer');
@@ -62,24 +67,34 @@ function curves_init(point_positions) {
         })
         .on('drag', dragmove);
 
-    //elementi draggabili, funzione che controlla il drag
+    //elementi draggabili, funzione che controlla il drag delle maniglie
     function dragmove(d) {
         // d.x = d3.event.x;
         d.y = d3.event.y;
-        d3.select(this).attr({
-            // cx: d.x,
-            cy: d.y
-        });
-        d.pathElem.attr('d', pathData);
-        if (d.controlLineElem) {
-            d.controlLineElem.attr('d', controlLinePath);
-        }
-        handleTextLayer.selectAll('text.handle-text.path' + d.pathID + '.p' + (d.handleID + 1))
-            .attr({
-                // x: d.x,
-                y: d.y
-            }).text(handleText(d, d.handleID));
+        
+        // se il mouse è inferiore a un certo valore y, non eseguire la funzione
+        // da problemi, la curva si aggiorna comunque
+        // if (d.y > 300) {
+
+            d3.select(this).attr({
+                // cx: d.x,
+                cy: d.y
+            });
+
+            d.pathElem.attr('d', pathData);
+
+            if (d.controlLineElem) {
+                d.controlLineElem.attr('d', controlLinePath);
+            }
+            
+            handleTextLayer.selectAll('text.handle-text.path' + d.pathID + '.p' + (d.handleID + 1))
+                .attr({
+                    // x: d.x,
+                    y: d.y
+                }).text(handleText(d, d.handleID));
+        // }
     }
+
     show_curves(controlLineLayer, mainLayer, handleTextLayer, handleLayer, curves, drag);
 }
 
@@ -114,7 +129,7 @@ function pathData(d) {
     ' ', p[6].x - handleOffset, ' ', p[6].y, ' ', p[6].x, ' ', p[6].y
     ].join('');
 
-    console.log("curve", curve);
+    //console.log("curve", curve);
     return curve;
 }
 
@@ -167,6 +182,7 @@ function show_curves(controlLineLayer, mainLayer, handleTextLayer, handleLayer, 
         })
         .text(handleText);
     
+        //disegna i cerchietti
     handleLayer.selectAll('circle.handle.path' + i)
         .data(d.points).enter().append('circle')
         .attr({
@@ -188,6 +204,78 @@ function show_curves(controlLineLayer, mainLayer, handleTextLayer, handleLayer, 
         .call(drag);
     });
 }
+
+
+curves_init(point_positions);
+
+
+// ------ STORE JSON
+var inc = 0;
+var curves_data = [];
+
+function storeJSON() {
+    
+    var obj = {
+        "curveID" : inc,
+        "curve " : curve
+    }
+
+    console.log("Da storare nel JSON")
+    curves_data[inc] = obj;
+    console.log(curves_data);
+    inc++;
+}
+
+// ------ ANIMATE LINES
+function animateLines() {
+    var p = curva_arrivo;
+    ca = [
+        //x y
+        'M', p[0].x, ' ', p[0].y,
+        //x1 y1 x2 y2 x y
+        'C', p[0].x + handleOffset, ' ', p[0].y, ' ', p[1].x - handleOffset, ' ', p[1].y, ' ', p[1].x, ' ', p[1].y,
+        //x2 y2 x y
+        'S', p[2].x - handleOffset, ' ', p[2].y, ' ', p[2].x, ' ', p[2].y, 
+        ' ', p[3].x - handleOffset, ' ', p[3].y, ' ', p[3].x, ' ', p[3].y, 
+        ' ', p[4].x - handleOffset, ' ', p[4].y, ' ', p[4].x, ' ', p[4].y,
+        ' ', p[5].x - handleOffset, ' ', p[5].y, ' ', p[5].x, ' ', p[5].y,
+        ' ', p[6].x - handleOffset, ' ', p[6].y, ' ', p[6].x, ' ', p[6].y
+        ].join('');
+
+        console.log(ca);
+
+    d3.select('curves')
+        .transition()
+        .duration(1000)
+        .attrTween('d', function () {
+            endPath = ca;
+            return endPath;
+            // return d3.morphPath(startPath, endPath);
+    });
+
+    $.each(json_data, function(i, item) {
+        point_positions = [];
+
+        line_response = json_data[i];
+        // var line_pi_id = line_response.line_pi_id;
+        var li_x = parseInt(line_response.x);
+        var li_y = parseInt(line_response.y);
+    
+        point_positions.push({
+            x: li_x,
+            y: li_y
+        })
+    })
+
+    console.log("fatto");
+    
+}
+
+
+//------------ DRAFT
+
+
+
 
 // //----Disegna le curve
 
@@ -251,7 +339,3 @@ function show_curves(controlLineLayer, mainLayer, handleTextLayer, handleLayer, 
 //                 .call(drag);
 //         });
 // }
-
-
-
-curves_init(point_positions);
